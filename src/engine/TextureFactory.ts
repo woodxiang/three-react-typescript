a878f80e460c037a8fb223ae0de0b4ae79ef859a
Xiang Dong <dong.xiang@live.com>
2021-03-11 17:03:41 +0800

    allow discrete color map

--- src/engine/TextureFactory.ts
+++ src/engine/TextureFactory.ts
@@ -2,9 +2,10 @@ import { Lut } from 'three/examples/jsm/math/Lut';
 import { RGBFormat } from 'three/src/constants';
 import { Color } from 'three/src/math/Color';
 import { DataTexture } from 'three/src/textures/DataTexture';
+import LutEx from './LutEx';
 
 export default class TextureFactory {
-  public static fromLut(lut: Lut): DataTexture {
+  public static fromLut(lut: Lut | LutEx): DataTexture {
     const width = lut.lut.length;
     const height = 1;
     const size = width;
@@ -20,15 +21,13 @@ export default class TextureFactory {
   }
 
   public static vertical1DFromColors(colors: Color[]): DataTexture {
-    const lut = new Lut();
-    lut.lut = colors;
-    const height = lut.lut.length;
+    const height = colors.length;
     const width = 1;
     const size = height;
 
     const data = new Uint8Array(size * 3);
     for (let i = 0; i < size; i += 1) {
-      const c = lut.lut[i];
+      const c = colors[i];
       data[i * 3] = c.r * 255;
       data[i * 3 + 1] = c.g * 255;
       data[i * 3 + 2] = c.b * 255;
